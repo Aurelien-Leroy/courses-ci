65259c6e7fbe9926d7cdfc6e524403983e977376
Leroy Aurelien <aurelienleroysn@gmail.com>
2024-11-16 00:26:46 +0100

    CI - 11

--- .github/workflows/index.js
+++ .github/workflows/index.js
@@ -3,22 +3,11 @@
 const express = require('express');
 const app = express();
 
-// Middleware de log avec Morgan pour les requêtes HTTP
-const morgan = require('morgan');
-app.use(morgan('combined')); // Log les requêtes HTTP en format "combined" (recommandé en production)
 
-// Route principale
 app.get('/', (req, res) => {
+  // Oubli de console.log, qui pourrait exposer des informations sensibles
   res.send('Hello World!');
 });
 
-// Catch-all pour les erreurs
-app.use((err, req, res, next) => {
-  console.error(err.stack); // Log l'erreur dans la console (en développement)
-  res.status(500).send('Something went wrong!');
-});
-
-// Démarrer le serveur
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
 });
