0eed762a150ee50c74fe9290b96d126d884c2f9b
Leroy Aurelien <aurelienleroysn@gmail.com>
2024-11-15 22:07:38 +0100

    CI - 6

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,18 @@
+const globals = require('globals');
+const pluginJs = require('@eslint/js');
+
+/** @type {import('eslint').Linter.Config[]} */
 module.exports = [
   {
     files: ['**/*.js'],
     languageOptions: {
       sourceType: 'commonjs',
-      ecmaVersion: 2021, // Assurez-vous d'utiliser une version compatible d'ECMAScript
-    },
-    rules: {
-      'constructor-super': 'off',
     },
   },
   {
     languageOptions: {
-      globals: require('globals').browser,
+      globals: globals.browser,
     },
   },
-  require('@eslint/js').configs.recommended,
+  pluginJs.configs.recommended,
 ];
