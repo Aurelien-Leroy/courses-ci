621ee1be4467e3db3a25fd2ddd41f37808c1b6de
Leroy Aurelien <aurelienleroysn@gmail.com>
2024-11-15 22:17:05 +0100

    CI - 6

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,10 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {
-    files: ["**/*.js"],
-    languageOptions: {
-      sourceType: "commonjs",
-    },
-    env: {
-      jest: true, // Active l'environnement Jest
-    },
-    plugins: ["jest"], // Ajouter le plugin jest
-    extends: ["plugin:jest/recommended"], // Appliquer les règles recommandées par Jest
-    rules: {
-      // Vous pouvez ajouter des règles personnalisées ici si nécessaire
-    },
-  },
-  {
-    languageOptions: {
-      globals: globals.browser,
-    },
-  },
-  configs.recommended,
+  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
 ];
