8458303f7f704d41d1ff6bd9a52c0d3977f44299
Leroy Aurelien <aurelienleroysn@gmail.com>
2024-11-15 23:40:20 +0100

    CI - 11

--- .github/workflows/index.js
+++ .github/workflows/index.js
@@ -7,10 +7,8 @@ const secret = "mySecretKey"; // Cette variable ne doit pas apparaître dans les
 
 app.get('/', (req, res) => {
   // Oubli de console.log, qui pourrait exposer des informations sensibles
-  console.log("Secret:", secret);  // Cette ligne ne devrait pas être dans le code de production
   res.send('Hello World!');
 });
 
 app.listen(3000, () => {
-  console.log('Server running on port 3000');
 });
