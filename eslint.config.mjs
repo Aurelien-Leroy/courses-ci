6713f75672b2cc481311f2bd9833cf87122dce30
Leroy Aurelien <aurelienleroysn@gmail.com>
2024-11-15 22:40:20 +0100

    CI - 6

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,10 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
+
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.jest, // Ajout des globals pour Jest
-      }
-    }
-  },
+  {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
 ];
